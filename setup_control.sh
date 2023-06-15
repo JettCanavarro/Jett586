2007e75fb74db2d88152cc1d2f120e80627fdbe7
JettCanavarro <98285768+JettCanavarro@users.noreply.github.com>
2023-06-15 19:41:46 -0400

    Update setup_control.sh

--- setup_control.sh
+++ setup_control.sh
@@ -3,4 +3,3 @@
 #generate passwordless SSH
 runuser -u jc997541 -- ssh-keygen -q -t rsa -f /users/jc997541/.ssh/id_rsa -N ''
 runuser -u jc997541 -- cat /users/jc997541/.ssh/id_rsa.pub >> /users/jc997541/.ssh/authorized_keys
-/users/lngo/.ssh/authorized_keys
