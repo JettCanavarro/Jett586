c229bd7b490236c69af19a2b8ecade7f5c68ea8d
JettCanavarro <98285768+JettCanavarro@users.noreply.github.com>
2023-06-15 21:16:15 -0400

    Update setup_host.sh

--- setup_host.sh
+++ setup_host.sh
@@ -4,7 +4,7 @@ apt update
 apt install -y nfs-common
 mkdir -p /var/nfs/keys
 
-while [! -f /var/nfs/keys/id_rsa ]; do
+while [ ! -f /var/nfs/keys/id_rsa ]; do
   mount 192.168.1.1:/var/nfs/keys /var/nfs/keys
   sleep 10
 done
