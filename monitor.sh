d91c2d3ca4f993ef2585651e66450218a73c6cad
JettCanavarro <98285768+JettCanavarro@users.noreply.github.com>
2023-06-30 21:03:45 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 ##check to see if the file exists
-if [ ! -f unauthorizedcopy.log ] 
+if [ ! -f unauthorizedcopy.log ]
 then
   ##check if unauthorized log has anything in it.
-  ulsize=$(stat -c %s "/var/webserver_log/unauthorized.log")
-  if [-z "ulsize" ] 
-  then 
+  ulsize=$(stat -c %s "/var/webserver_monitor/unauthorized.log")
+  if [ -z "ulsize" ]
+  then
     ##if the log is empty, no need to make the copy file, just send the email to admin
     sudo mail admin@pcvm435-2.emulab.net <<< "No Unauthorized Access"
-  else 
+  else
     ##if not make the copy file
-    sudo cp /var/webserver_log/unauthorized.log /var/webserver_log/unauthorizedcopy.log
+    sudo cp /var/webserver_monitor/unauthorized.log /var/webserver_monitor/unauthorizedcopy.log
     ## mail the contents of the new entries to the admin
     ## CHANGE EMAIL ADDRESS BEFORE SUBMITTING
-    sudo mail admin@pcvm435-2.emulab.net < unauthorizedcopy.log
+    sudo mail admin@pcvm435-2.emulab.net < /var/webserver_monitor/unauthorizedcopy.log
   fi
 else
   ##if it does exist, then check if there are differences
@@ -24,13 +24,12 @@ else
     echo $(sudo diff unauthorized.log unauthorizedcopy.log | grep -oP '(?<=>).*') >> mail.log
     sudo mail admin@pcvm435-2.emulab.net < mail.log
     ##copy unauthorized log over to replace the copy file
-    sudo cp /var/webserver_log/unauthorized.log /var/webserver_log/unauthorizedcopy.log
+    sudo cp /var/webserver_monitor/unauthorized.log /var/webserver_monitor/unauthorizedcopy.log
   else
     ##if there are not differences then send the proper email
     sudo mail admin@pcvm435-2.emulab.net <<< "No Unauthorized Access"
-  fi 
+  fi
 fi
-  
     
 
 
