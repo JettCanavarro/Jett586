e6a89d2c4987fa59016022044b6e26bdd4ee10e7
JettCanavarro <98285768+JettCanavarro@users.noreply.github.com>
2023-06-30 20:29:57 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -3,10 +3,11 @@ invalidUsers= $(sudo grep 'invalid-user' /var/log/auth.log.1)
 
 #scan for invalid user
 
-if [-z $invalidUsers]
+if [ -z "$invalidUsers" ]
 then
-echo "$(curl --silent "https://ipinfo.io/ip") $(curl --silent "https://ipinfo.io/country") $(sudo
-grep -o "[a-Z]\{3\} [0-9]\{2\}" /var/log/auth.log.1)" | sudo tee /var/webserver_log/authorized_log
+  ##this will append the lines in the proper format.
+  echo "$(curl --silent "https://ipinfo.io/ip") $(curl --silent "https://ipinfo.io/country") $(sudo
+  grep -o "[a-Z]\{3\} [0-9]\{2\}" /var/log/auth.log.1)" | sudo tee /var/webserver_log/authorized_log
 fi
 
 ##get date
