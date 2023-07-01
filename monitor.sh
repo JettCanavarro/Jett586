e6279a1df7628c9689527307bc8b67ab848100bd
JettCanavarro <98285768+JettCanavarro@users.noreply.github.com>
2023-06-30 20:27:31 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -3,24 +3,33 @@
 ##check to see if the file exists
 if [ ! -f unauthorizedcopy.log ] 
 then
-  ##if not make the copy file
-  sudo mv /var/webserver_log/unauthorized.log /var/webserver_log/unauthorizedcopy.log
-  ## mail the contents of the new entries to the admin
-  ## CHANGE EMAIL ADDRESS BEFORE SUBMITTING
-  sudo mail admin@pcvm435-2.emulab.net < unauthorizedcopy.log
-else
-  ##if it does exist, then check if the unauthorized copy log is bigger than unauthorized log
-  ucsize=$(stat -c %s "/var/webserver_log/unauthorizedcopy.log")
+  ##check if unauthorized log has anything in it.
   ulsize=$(stat -c %s "/var/webserver_log/unauthorized.log")
-  ## if they are then send the differences
+  if [-z "ulsize" ] 
+  then 
+    ##if the log is empty, no need to make the copy file, just send the email to admin
+    sudo mail admin@pcvm435-2.emulab.net <<< "No Unauthorized Access"
+  else 
+    ##if not make the copy file
+    sudo cp /var/webserver_log/unauthorized.log /var/webserver_log/unauthorizedcopy.log
+    ## mail the contents of the new entries to the admin
+    ## CHANGE EMAIL ADDRESS BEFORE SUBMITTING
+    sudo mail admin@pcvm435-2.emulab.net < unauthorizedcopy.log
+  fi
+else
+  ##if it does exist, then check if there are differences
   if sudo diff unauthorized.log unauthorizedcopy.log
   then
+    ## if there are then send the differences to admin
     echo $(sudo diff unauthorized.log unauthorizedcopy.log | grep -oP '(?<=>).*') >> mail.log
     sudo mail admin@pcvm435-2.emulab.net < mail.log
-    sudo cat unauthorized.log unauthorizedcopy.log
+    ##copy unauthorized log over to replace the copy file
+    sudo cp /var/webserver_log/unauthorized.log /var/webserver_log/unauthorizedcopy.log
   else
+    ##if there are not differences then send the proper email
     sudo mail admin@pcvm435-2.emulab.net <<< "No Unauthorized Access"
-  fi  
+  fi 
+fi
   
     
 
